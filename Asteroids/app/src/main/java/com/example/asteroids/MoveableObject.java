43972f53ea1bb5ff19c2f2474d4d077a6e3088ca
Kyle Muldoon <kylemuldoon15@gmail.com>
2019-07-11 17:09:18 -0700

    wrote outline for moveable object

--- Asteroids/app/src/main/java/com/example/asteroids/MoveableObject.java
+++ Asteroids/app/src/main/java/com/example/asteroids/MoveableObject.java
@@ -3,11 +3,79 @@ package com.example.asteroids;
 // AUTHOR NAME HERE
 
 abstract class MoveableObject {
+    ///////////////////////////
+    //      VARIABLES
+    ///////////////////////////
 
-    // VARIABLES
+    // position / direction / speed / physics
+    protected int posX;
+    protected int posY;
+    protected int mass;
+    protected int maxVelocity;
+    protected int minVelocity;
+    protected float drctnVector;
 
-    // CONSTRUCTOR
+    // shape
+    protected int[] shape;
 
-    // METHODS
+    ///////////////////////////
+    //      METHODS
+    ///////////////////////////
 
+    // getters / setters
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
+
+    public int getMaxVelocity() {
+        return maxVelocity;
+    }
+
+    public void setMaxVelocity(int maxVelocity) {
+        this.maxVelocity = maxVelocity;
+    }
+
+    public int getMinVelocity() {
+        return minVelocity;
+    }
+
+    public void setMinVelocity(int minVelocity) {
+        this.minVelocity = minVelocity;
+    }
+
+    public float getDrctnVector() {
+        return drctnVector;
+    }
+
+    public void setDrctnVector(float drctnVector) {
+        this.drctnVector = drctnVector;
+    }
+
+    public int[] getShape() {
+        return shape;
+    }
+
+    public void setShape(int[] shape) {
+        this.shape = shape;
+    }
 }
