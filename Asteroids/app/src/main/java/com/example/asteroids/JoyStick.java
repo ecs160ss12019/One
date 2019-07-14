9478d8b18693b216ef2b9ee89812e55077b169ec
Kyle Muldoon <kylemuldoon15@gmail.com>
2019-07-14 16:18:27 -0700

    Joystick now ready for testing. done now unless bugs are found

--- Asteroids/app/src/main/java/com/example/asteroids/JoyStick.java
+++ Asteroids/app/src/main/java/com/example/asteroids/JoyStick.java
@@ -44,13 +44,17 @@ public class JoyStick {
     ///////////////////////////
     //      METHODS
     ///////////////////////////
-    public PointF getStickPosition() {
-        return this.stickPosition;
-    }
 
     // TODO: make mapped stick functionality.
     public PointF getScaledStickPosition() {
-        return null;
+        // this method returns the percentage of how far the stick is engaged in x-y directions
+        // bounds are [-100f - +100f], where -100f means -100% engaged on the axis and +100f means
+        // %100% engaged on the axis.
+
+        float xEngaged = 100 * (stickPosition.x / baseRadius);
+        float yEngaged = 100 * (stickPosition.y / baseRadius);
+        PointF scaledOutput = new PointF(xEngaged, yEngaged);
+        return scaledOutput;
     }
 
     public void setTouchStatus(boolean temp) {
@@ -65,14 +69,14 @@ public class JoyStick {
             stickPosition.y = y;
 
             // find magnitude of displacement from center of base circle
-            float displacement = (float) Math.sqrt(Math.pow((stickPosition.x - baseCenter.x), 2) + Math.pow((stickPosition.y - baseCenter.y), 2));
+            float stickDisplacement = (float) Math.sqrt(Math.pow((stickPosition.x - baseCenter.x), 2) + Math.pow((stickPosition.y - baseCenter.y), 2));
 
-            if (displacement < baseRadius) {
+            if (stickDisplacement < baseRadius) {
                 // when hat does't need constraining to base radius
                 hatCenter = stickPosition;
             } else {
                 // when hat needs constraining to base radius
-                float ratio = baseRadius / displacement;
+                float ratio = baseRadius / stickDisplacement;
                 float constrainedX = baseCenter.x + (stickPosition.x - baseCenter.x) * ratio;
                 float constrainedY = baseCenter.y + (stickPosition.y - baseCenter.y) * ratio;
                 stickPosition.x = constrainedX;
@@ -81,38 +85,7 @@ public class JoyStick {
         }
     }
 
-    /*
-        public void draw(float newX, float newY) {
-            if (getHolder().getSurface().isValid()) {
-                Canvas myCanvas = this.getHolder().lockCanvas(); //Stuff to draw
-                Paint colors = new Paint();
-                myCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR); // Clear the BG
 
-                //First determine the sin and cos of the angle that the touched point is at relative to the baseCenter of the joystick
-                float hypotenuse = (float) Math.sqrt(Math.pow(newX - centerX, 2) + Math.pow(newY - centerY, 2));
-                float sin = (newY - centerY) / hypotenuse; //sin = o/h
-                float cos = (newX - centerX) / hypotenuse; //cos = a/h
-
-                //Draw the base first before shading
-                colors.setARGB(255, 100, 100, 100);
-                myCanvas.drawCircle(centerX, centerY, baseRadius, colors);
-                for (int i = 1; i <= (int) (baseRadius / ratio); i++) {
-                    colors.setARGB(150 / i, 255, 0, 0); //Gradually decrease the shade of black drawn to create a nice shading effect
-                    myCanvas.drawCircle(newX - cos * hypotenuse * (ratio / baseRadius) * i,
-                            newY - sin * hypotenuse * (ratio / baseRadius) * i, i * (hatRadius * ratio / baseRadius), colors); //Gradually increase the size of the shading effect
-                }
-
-                //Drawing the joystick hat
-                for (int i = 0; i <= (int) (hatRadius / ratio); i++) {
-                    colors.setARGB(255, (int) (i * (255 * ratio / hatRadius)), (int) (i * (255 * ratio / hatRadius)), 255); //Change the joystick color for shading purposes
-                    myCanvas.drawCircle(newX, newY, hatRadius - (float) i * (ratio) / 2, colors); //Draw the shading for the hat
-                }
-
-                getHolder().unlockCanvasAndPost(myCanvas); //Write the new drawing to the SurfaceView
-            }
-        }
-
-    */
     public Path[] draw() {
         //get current position of hat, then move it
 
