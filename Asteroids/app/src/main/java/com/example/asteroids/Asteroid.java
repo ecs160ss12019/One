4441a2108de7818f0001d80e75959e2c3adeea52
bcoe <briancoe@gmail.com>
2019-07-13 22:26:37 -0700

    added coordinates for asteroids, arrays of points to hold each, and a method to populate the arrays

--- Asteroids/app/src/main/java/com/example/asteroids/Asteroid.java
+++ Asteroids/app/src/main/java/com/example/asteroids/Asteroid.java
@@ -3,13 +3,23 @@ package com.example.asteroids;
 // AUTHOR NAME HERE
 
 import android.graphics.Path;
+import android.graphics.Point;
 
 public class Asteroid extends MovableObject implements Drawable {
 
     ///////////////////////////
     //      VARIABLES
     ///////////////////////////
-
+    private int scalar;
+    private Point[] ab1;
+    private Point[] ab2;
+    private Point[] ab3;
+    private Point[] am1;
+    private Point[] am2;
+    private Point[] am3;
+    private Point[] as1;
+    private Point[] as2;
+    private Point[] as3;
 
     ///////////////////////////
     //      CONSTRUCTOR
@@ -19,13 +29,74 @@ public class Asteroid extends MovableObject implements Drawable {
         // posX, posY, mass, maxVelocity, minVelocity, drctnVector, shape
         super();
 
-
     }
 
     ///////////////////////////
     //      METHODS
     ///////////////////////////
 
+    private void populate() {
+        ab1[0] = new Point(0,0);
+        ab1[1] = new Point(5,1);
+        ab1[2] = new Point(7,6);
+        ab1[3] = new Point(4,5);
+        ab1[4] = new Point(5,8);
+        ab1[5] = new Point(1,7);
+        ab1[6] = new Point(2,3);
+
+        ab2[0] = new Point(2,0);
+        ab2[1] = new Point(4,1);
+        ab2[2] = new Point(6,0);
+        ab2[3] = new Point(8,2);
+        ab2[4] = new Point(7,6);
+        ab2[5] = new Point(4,5);
+        ab2[6] = new Point(1,6);
+        ab2[7] = new Point(0,2);
+
+        ab3[0] = new Point(4,0);
+        ab3[1] = new Point(8,1);
+        ab3[2] = new Point(7,7);
+        ab3[3] = new Point(3,3);
+        ab3[4] = new Point(0,4);
+
+        am1[0] = new Point(0,1);
+        am1[1] = new Point(2,2);
+        am1[2] = new Point(1,0);
+        am1[3] = new Point(4,1);
+        am1[4] = new Point(4,3);
+        am1[5] = new Point(3,4);
+        am1[6] = new Point(1,4);
+
+        am2[0] = new Point(0,0);
+        am2[1] = new Point(3,1);
+        am2[2] = new Point(2,2);
+        am2[3] = new Point(4,4);
+        am2[4] = new Point(1,5);
+
+        am3[0] = new Point(0,2);
+        am3[1] = new Point(2,0);
+        am3[2] = new Point(4,1);
+        am3[3] = new Point(3,4);
+        am3[4] = new Point(1,4);
+
+        as1[0] = new Point(0,0);
+        as1[1] = new Point(3,1);
+        as1[2] = new Point(2,2);
+        as1[3] = new Point(0,2);
+
+        as2[0] = new Point(0,0);
+        as2[1] = new Point(2,0);
+        as2[2] = new Point(3,3);
+        as2[3] = new Point(0,2);
+        as2[4] = new Point(1,1);
+
+        as3[0] = new Point(0,1);
+        as3[1] = new Point(1,0);
+        as3[2] = new Point(3,2);
+        as3[3] = new Point(2,2);
+        as3[4] = new Point(1,3);
+        as3[5] = new Point(1,2);
+    }
     public Path draw() {
         return shape;
     }
