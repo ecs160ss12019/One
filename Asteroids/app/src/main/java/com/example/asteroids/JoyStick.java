a49f794b20d60c2887e26be8d58f3558118ff8ba
Martin Petrov <mlpetrov@ucdavis.edu>
2019-07-14 23:28:02 -0700

    cleaned up joystick.java

--- Asteroids/app/src/main/java/com/example/asteroids/JoyStick.java
+++ Asteroids/app/src/main/java/com/example/asteroids/JoyStick.java
@@ -27,18 +27,19 @@ public class JoyStick {
 
     public JoyStick(PointF blockSize) {
 
-        baseCenter = new PointF(50 * blockSize.x, 50  * blockSize.y);
+        //NOTE: Just trust that base center must have blockSize.y / blockSize.x. Long story
+        baseCenter = new PointF(70, 40 * blockSize.y / blockSize.x);
         stickPosition = new PointF(0,0);
 
         base = new Path();
         hat = new Path();
 
         this.blockSize = blockSize;
-        baseRadius = (int) (20 * blockSize.x);
-        hatRadius = (int) (8 * blockSize.x);
+        baseRadius = 20;
+        hatRadius = 8;
 
-        base.addCircle(baseCenter.x, baseCenter.y, baseRadius, Path.Direction.CW);
-        hat.addCircle(baseCenter.x, baseCenter.y, hatRadius, Path.Direction.CW);
+        base.addCircle(baseCenter.x * blockSize.x, baseCenter.y * blockSize.x, baseRadius * blockSize.x, Path.Direction.CW);
+        hat.addCircle(baseCenter.x * blockSize.x, baseCenter.y * blockSize.x, hatRadius * blockSize.x, Path.Direction.CW);
 
     }
 
@@ -46,15 +47,13 @@ public class JoyStick {
     //      METHODS
     ///////////////////////////
 
-    // TODO: make mapped stick functionality.
     public PointF getScaledStickPosition() {
         // this method returns the percentage of how far the stick is engaged in x-y directions
         // bounds are [-100f - +100f], where -100f means -100% engaged on the axis and +100f means
         // %100% engaged on the axis.
 
-
-        float xEngaged = (stickPosition.x - baseCenter.x / blockSize.x ) / (baseRadius / blockSize.x);
-        float yEngaged = (stickPosition.y - baseCenter.y / blockSize.x ) / (baseRadius  / blockSize.x);
+        float xEngaged = (stickPosition.x - baseCenter.x ) / (baseRadius);
+        float yEngaged = (stickPosition.y - baseCenter.y ) / (baseRadius);
 
 
         PointF scaledOutput = new PointF(xEngaged, yEngaged);
@@ -63,38 +62,38 @@ public class JoyStick {
 
     public void resetJoyStick() {
         hat.reset();
-        hat.addCircle(baseCenter.x, baseCenter.y, hatRadius, Path.Direction.CW);
+        hat.addCircle(baseCenter.x * blockSize.x, baseCenter.y * blockSize.x, hatRadius * blockSize.x, Path.Direction.CW);
     }
 
     //X and Y have values of 0-100 mapped to domain of screen
     public void updateStick(float x, float y) {
 
         //distance of touch from joystick
-        float xCentered = x - baseCenter.x / blockSize.x;
-        float yCentered = y - baseCenter.y / blockSize.x;
+        float xCentered = x - baseCenter.x;
+        float yCentered = y - baseCenter.y;
 
         //TODO: add bound on where you can touch joystick from w/ simple if statement
 
         //distance from the center of joystick
         float distanceFromCenter = (float) Math.sqrt(Math.pow(xCentered, 2 ) + Math.pow(yCentered, 2));
 
-        if (distanceFromCenter < baseRadius / blockSize.x) { //If the touch is in bounds of joystick
+        if (distanceFromCenter < baseRadius) { //If the touch is in bounds of joystick
 
             stickPosition.x = x;
             stickPosition.y = y;
 
         } else { // when hat needs constraining to base radius
-            float ratio = baseRadius / blockSize.x / distanceFromCenter ;
+            float ratio = baseRadius / distanceFromCenter ;
 
-            float constrainedX = baseCenter.x / blockSize.x + (xCentered) * ratio;
-            float constrainedY = baseCenter.y / blockSize.x + (yCentered) * ratio;
+            float constrainedX = baseCenter.x + (xCentered) * ratio;
+            float constrainedY = baseCenter.y + (yCentered) * ratio;
             stickPosition.x = constrainedX;
             stickPosition.y = constrainedY;
         }
 
         //Reset path, add a new circle with the new coordinates
         hat.reset();
-        hat.addCircle(stickPosition.x * blockSize.x ,stickPosition.y * blockSize.x, hatRadius, Path.Direction.CW);
+        hat.addCircle(stickPosition.x * blockSize.x ,stickPosition.y * blockSize.x, hatRadius * blockSize.x, Path.Direction.CW);
 
     }
 
