0de91017e586023b8dad2b2532809d7fe42e989f
bcoe <briancoe@gmail.com>
2019-07-14 17:38:19 -0700

    updated asteroid gen algortihm

--- Asteroids/app/src/main/java/com/example/asteroids/Asteroid.java
+++ Asteroids/app/src/main/java/com/example/asteroids/Asteroid.java
@@ -21,6 +21,7 @@ public class Asteroid extends MovableObject {
     private Point[] as1 = new Point[4];
     private Point[] as2 = new Point[5];
     private Point[] as3 = new Point[6];
+    private Point[] currentAsteroid;
     private int numHits;
     private int asteroidType;
 
@@ -31,7 +32,7 @@ public class Asteroid extends MovableObject {
     public Asteroid() {
         // posX, posY, mass, maxVelocity, minVelocity, drctnVector, shape
         super();
-        populate();
+        currentAsteroid = populate();
         Random random = new Random();
         asteroidType = random.nextInt(9) + 1;
     }
@@ -41,126 +42,87 @@ public class Asteroid extends MovableObject {
     ///////////////////////////
 
     public Path draw() {
-
-        switch(asteroidType){
-            case 1:
-                shape.moveTo(ab1[0].x*scalar+300,ab1[0].y*scalar+400);
-                for(int i = 1; i < ab1.length; i++){
-                    shape.lineTo(ab1[i].x*scalar+300,ab1[i].y*scalar+400);
+                shape.moveTo(currentAsteroid[0].x*scalar+300,currentAsteroid[0].y*scalar+400);
+                for(int i = 1; i < currentAsteroid.length; i++){
+                    shape.lineTo(currentAsteroid[i].x*scalar+300,currentAsteroid[i].y*scalar+400);
                 }
-                break;
-            case 2:
-                shape.moveTo(ab2[0].x*scalar+300,ab2[0].y*scalar+400);
-                for(int i = 1; i < ab2.length; i++){
-                    shape.lineTo(ab2[i].x*scalar+300,ab2[i].y*scalar+400);
-                }
-                break;
-            case 3:
-                shape.moveTo(ab3[0].x*scalar+300,ab3[0].y*scalar+400);
-                for(int i = 1; i < ab3.length; i++){
-                    shape.lineTo(ab3[i].x*scalar+300,ab3[i].y*scalar+400);
-                }
-                break;
-            case 4:
-                shape.moveTo(am1[0].x*scalar+300,am1[0].y*scalar+400);
-                for(int i = 1; i < am1.length; i++){
-                    shape.lineTo(am1[i].x*scalar+300,am1[i].y*scalar+400);
-                }
-                break;
-            case 5:
-                shape.moveTo(am2[0].x*scalar+300,am2[0].y*scalar+400);
-                for(int i = 1; i < am2.length; i++){
-                    shape.lineTo(am2[i].x*scalar+300,am2[i].y*scalar+400);
-                }
-                break;
-            case 6:
-                shape.moveTo(am3[0].x*scalar+300,am3[0].y*scalar+400);
-                for(int i = 1; i < am3.length; i++){
-                    shape.lineTo(am3[i].x*scalar+300,am3[i].y*scalar+400);
-                }
-                break;
-            case 7:
-                shape.moveTo(as1[0].x*scalar+300,as1[0].y*scalar+400);
-                for(int i = 1; i < as1.length; i++){
-                    shape.lineTo(as1[i].x*scalar+300,as1[i].y*scalar+400);
-                }
-                break;
-            case 8:
-                shape.moveTo(as2[0].x*scalar+300,as2[0].y*scalar+400);
-                for(int i = 1; i < as2.length; i++){
-                    shape.lineTo(as2[i].x*scalar+300,as2[i].y*scalar+400);
-                }
-                break;
-            case 9:
-                shape.moveTo(as3[0].x*scalar+300,as3[0].y*scalar+400);
-                for(int i = 1; i < as3.length; i++){
-                    shape.lineTo(as3[i].x*scalar+300,as3[i].y*scalar+400);
-                }
-                break;
-        }
         return shape;
     }
 
-    private void populate() {
-        ab1[0] = new Point(0,0);
-        ab1[1] = new Point(5,1);
-        ab1[2] = new Point(7,6);
-        ab1[3] = new Point(4,5);
-        ab1[4] = new Point(5,8);
-        ab1[5] = new Point(1,7);
-        ab1[6] = new Point(2,3);
 
-        ab2[0] = new Point(2,0);
-        ab2[1] = new Point(4,1);
-        ab2[2] = new Point(6,0);
-        ab2[3] = new Point(8,2);
-        ab2[4] = new Point(7,6);
-        ab2[5] = new Point(4,5);
-        ab2[6] = new Point(1,6);
-        ab2[7] = new Point(0,2);
-
-        ab3[0] = new Point(4,0);
-        ab3[1] = new Point(8,1);
-        ab3[2] = new Point(7,7);
-        ab3[3] = new Point(3,3);
-        ab3[4] = new Point(0,4);
-
-        am1[0] = new Point(0,1);
-        am1[1] = new Point(2,2);
-        am1[2] = new Point(1,0);
-        am1[3] = new Point(4,1);
-        am1[4] = new Point(4,3);
-        am1[5] = new Point(3,4);
-        am1[6] = new Point(1,4);
-
-        am2[0] = new Point(0,0);
-        am2[1] = new Point(3,1);
-        am2[2] = new Point(2,2);
-        am2[3] = new Point(4,4);
-        am2[4] = new Point(1,5);
-
-        am3[0] = new Point(0,2);
-        am3[1] = new Point(2,0);
-        am3[2] = new Point(4,1);
-        am3[3] = new Point(3,4);
-        am3[4] = new Point(1,4);
-
-        as1[0] = new Point(0,0);
-        as1[1] = new Point(3,1);
-        as1[2] = new Point(2,2);
-        as1[3] = new Point(0,2);
-
-        as2[0] = new Point(0,0);
-        as2[1] = new Point(2,0);
-        as2[2] = new Point(3,3);
-        as2[3] = new Point(0,2);
-        as2[4] = new Point(1,1);
-
-        as3[0] = new Point(0,1);
-        as3[1] = new Point(1,0);
-        as3[2] = new Point(3,2);
-        as3[3] = new Point(2,2);
-        as3[4] = new Point(1,3);
-        as3[5] = new Point(1,2);
+    private Point[] populate() {
+        switch(asteroidType){
+            case 1:
+                ab1[0] = new Point(0,0);
+                ab1[1] = new Point(5,1);
+                ab1[2] = new Point(7,6);
+                ab1[3] = new Point(4,5);
+                ab1[4] = new Point(5,8);
+                ab1[5] = new Point(1,7);
+                ab1[6] = new Point(2,3);
+                return ab1;
+            case 2:
+                ab2[0] = new Point(2,0);
+                ab2[1] = new Point(4,1);
+                ab2[2] = new Point(6,0);
+                ab2[3] = new Point(8,2);
+                ab2[4] = new Point(7,6);
+                ab2[5] = new Point(4,5);
+                ab2[6] = new Point(1,6);
+                ab2[7] = new Point(0,2);
+                return ab2;
+            case 3:
+                ab3[0] = new Point(4,0);
+                ab3[1] = new Point(8,1);
+                ab3[2] = new Point(7,7);
+                ab3[3] = new Point(3,3);
+                ab3[4] = new Point(0,4);
+                return ab3;
+            case 4:
+                am1[0] = new Point(0,1);
+                am1[1] = new Point(2,2);
+                am1[2] = new Point(1,0);
+                am1[3] = new Point(4,1);
+                am1[4] = new Point(4,3);
+                am1[5] = new Point(3,4);
+                am1[6] = new Point(1,4);
+                return am1;
+            case 5:
+                am2[0] = new Point(0,0);
+                am2[1] = new Point(3,1);
+                am2[2] = new Point(2,2);
+                am2[3] = new Point(4,4);
+                am2[4] = new Point(1,5);
+                return am2;
+            case 6:
+                am3[0] = new Point(0,2);
+                am3[1] = new Point(2,0);
+                am3[2] = new Point(4,1);
+                am3[3] = new Point(3,4);
+                am3[4] = new Point(1,4);
+                return am3;
+            case 7:
+                as1[0] = new Point(0,0);
+                as1[1] = new Point(3,1);
+                as1[2] = new Point(2,2);
+                as1[3] = new Point(0,2);
+                return as1;
+            case 8:
+                as2[0] = new Point(0,0);
+                as2[1] = new Point(2,0);
+                as2[2] = new Point(3,3);
+                as2[3] = new Point(0,2);
+                as2[4] = new Point(1,1);
+                return as2;
+            case 9:
+                as3[0] = new Point(0,1);
+                as3[1] = new Point(1,0);
+                as3[2] = new Point(3,2);
+                as3[3] = new Point(2,2);
+                as3[4] = new Point(1,3);
+                as3[5] = new Point(1,2);
+                return as3;
+        }
+        return currentAsteroid;
     }
 }
