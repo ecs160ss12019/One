c592e4506466fab3e41a8ba36338721fe87bc4a7
rhinus <briancoe@gmail.com>
2019-07-17 10:55:14 -0700

    hopefully finished the asteroid refactoring

--- Asteroids/app/src/main/java/com/example/asteroids/Asteroid.java
+++ Asteroids/app/src/main/java/com/example/asteroids/Asteroid.java
@@ -3,13 +3,19 @@ package com.example.asteroids;
 import android.graphics.Point;
 
 public class Asteroid {
+    private AsteroidGenerator aGen;
     public Point dVect;
     public long  time;
     public long startTime;
     public long curTime;
     public Point offSet;
     public int asteroidType;
+    public Point[] image;
     public Point[] newPos;
 
+    public void setAsteroid(int asteroidType){
+        this.asteroidType = asteroidType;
+        this.image = aGen.which(this.asteroidType);
+    }
 
 }
