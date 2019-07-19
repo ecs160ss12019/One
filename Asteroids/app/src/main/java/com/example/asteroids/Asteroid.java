6566c943497c1f40db4f31442d8f92a7a9304132
rhinus <briancoe@gmail.com>
2019-07-19 11:39:28 -0700

    refactored several variables from Asteroid manager into asteroid, asteroid now handles much of its own calculations. further refactoring coming, to better use movableobject abstract class

--- Asteroids/app/src/main/java/com/example/asteroids/Asteroid.java
+++ Asteroids/app/src/main/java/com/example/asteroids/Asteroid.java
@@ -1,6 +1,7 @@
 package com.example.asteroids;
 
 import android.graphics.Point;
+import java.util.Random;
 
 public class Asteroid {
     private AsteroidGenerator aGen = new AsteroidGenerator();
@@ -10,7 +11,11 @@ public class Asteroid {
     public long curTime;
     public Point offSet;
     public int asteroidType;
+    public boolean reDraw = true;
     private int maxVertices = 10;
+    private int scalar = 15;
+    private int resX;
+    private int resY;
     public Point[] image;
     public Point[] newPos = new Point[maxVertices];
 
@@ -18,5 +23,76 @@ public class Asteroid {
         this.asteroidType = asteroidType;
         this.image = aGen.which(this.asteroidType);
     }
+    public void checkReDraw(){
+        for (int i = 0; i < image.length; i++) {
+            if(newPos[i].x < -resX/4 || newPos[i].y < -resY/4
+                    || newPos[i].x > resX + resX/4 || newPos[i].y > resY + resY/4)
+                reDraw = true;
+        }
+    }
+    
+    public void setNewPos(){
+        checkReDraw();
+        curTime = System.nanoTime() / 1000000;
+        time = curTime - startTime;
+        if(image != null) {
+            newPos[0].x = (int) (image[0].x * scalar + offSet.x + dVect.x *time/100);
+            newPos[0].y = (int) (image[0].y * scalar + offSet.y + dVect.y *time/100);
+            for (int i = 1; i < image.length; i++) {
+                newPos[i].x = (int) (image[i].x * scalar + offSet.x + dVect.x *time/100);
+                newPos[i].y = (int) (image[i].y * scalar + offSet.y + dVect.y *time/100);
+            }
+        }
+    }
 
+    public void initAsteroid(int resX, int resY){
+        reDraw = false;
+        this.resX = resX;
+        this.resY = resY;
+        startTime = System.nanoTime() / 1000000;
+        curTime = startTime;
+        setAsteroid(new Random().nextInt(9) + 1);
+        offSet = randOffset();
+        dVect = randDirection(offSet);
+        time = 0;
+    }
+    private Point randOffset(){
+        // pick where to generate
+        Point tempOffSet = new Point();
+        switch(new Random().nextInt(4)) {
+            case 0:
+                tempOffSet.x = new Random().nextInt(resX);
+                tempOffSet.y = 0;
+                break;
+            case 1:
+                tempOffSet.x = new Random().nextInt(resX);
+                tempOffSet.y = resY;
+                break;
+            case 2:
+                tempOffSet.x = 0;
+                tempOffSet.y = new Random().nextInt(resY);
+                break;
+            case 3:
+                tempOffSet.x = resX;
+                tempOffSet.y = new Random().nextInt(resY);
+                break;
+        }
+        return tempOffSet;
+    }
+
+    private Point randDirection(Point oSet){
+        // pick where to generate
+        Point tempVect = new Point();
+        if(oSet.x <= resX/2){
+            tempVect.x = new Random().nextInt(resY / (scalar*4));
+        }else if(oSet.x > resX/2) {
+            tempVect.x = -new Random().nextInt(resX / (scalar*4));
+        }
+        if(oSet.y <= resY/2) {
+            tempVect.y = new Random().nextInt(resY / (scalar*5));
+        }else if(oSet.y > resY/2) {
+            tempVect.y = -new Random().nextInt(resX / (scalar*5));
+        }
+        return tempVect;
+    }
 }
--- Asteroids/app/src/main/java/com/example/asteroids/AsteroidManager.java
+++ Asteroids/app/src/main/java/com/example/asteroids/AsteroidManager.java
@@ -14,8 +14,6 @@ public class AsteroidManager extends MovableObject {
     ///////////////////////////
     //      VARIABLES
     ///////////////////////////
-    private int scalar = 15;
-    Random random = new Random();
     private Vector<Asteroid> asteroidTracker = new Vector<Asteroid>();
 //    private Asteroid currentAsteroid;
     private int numAsteroids = 10;
@@ -60,87 +58,20 @@ public class AsteroidManager extends MovableObject {
         shape.rewind();
         genAsteroid();
         for(Asteroid ast:asteroidTracker){
-            ast.curTime = System.nanoTime() / 1000000;
-            ast.time = ast.curTime - ast.startTime;
-            if(ast.image != null) {
-                ast.newPos[0].x = (int) (ast.image[0].x * scalar + ast.offSet.x + ast.dVect.x *ast.time/100);
-                ast.newPos[0].y = (int) (ast.image[0].y * scalar + ast.offSet.y + ast.dVect.y *ast.time/100);
+            ast.setNewPos();
                 shape.moveTo(ast.newPos[0].x, ast.newPos[0].y);
                 for (int i = 1; i < ast.image.length; i++) {
-                    ast.newPos[i].x = (int) (ast.image[i].x * scalar + ast.offSet.x + ast.dVect.x *ast.time/100);
-                    ast.newPos[i].y = (int) (ast.image[i].y * scalar + ast.offSet.y + ast.dVect.y *ast.time/100);
                     shape.lineTo(ast.newPos[i].x, ast.newPos[i].y);
                 }
             }
-        }
         return shape;
     }
 
     private void genAsteroid(){
         for(Asteroid ast: asteroidTracker){
-            boolean reDraw = false;
-            if(!firstRun || ast.image != null) {
-                for (int i = 0; i < ast.image.length; i++) {
-                    if(
-                            (int) (ast.image[i].x * scalar + ast.offSet.x + ast.dVect.x * ast.time/100) < -resX/4
-                            || (int) (ast.image[i].y * scalar + ast.offSet.y + ast.dVect.y * ast.time/100) < -resY/4
-                            || (int) (ast.image[i].x * scalar + ast.offSet.x + ast.dVect.x * ast.time/100) > resX + resX/4
-                            || (int) (ast.image[i].y * scalar + ast.offSet.y + ast.dVect.y * ast.time/100) > resY + resY/4)
-                        reDraw = true;
-                }
-            }
-            if(firstRun || reDraw){
-                ast.startTime = System.nanoTime() / 1000000;
-                ast.curTime = ast.startTime;
-                ast.setAsteroid(random.nextInt(9) + 1);
-                ast.offSet = randOffset();
-                ast.dVect = randDirection(ast.offSet);
-                ast.time = 0;
-            }
+            if(ast.reDraw)
+                ast.initAsteroid(resX, resY);
         }
-        firstRun = false;
-    }
-
-    private Point randOffset(){
-        // pick where to generate
-        Point tempOffSet = new Point();
-        int rand = random.nextInt(4);
-        switch(rand) {
-            case 0:
-                tempOffSet.x = random.nextInt(resX);
-                tempOffSet.y = 0;
-                break;
-            case 1:
-                tempOffSet.x = random.nextInt(resX);
-                tempOffSet.y = resY;
-                break;
-            case 2:
-                tempOffSet.x = 0;
-                tempOffSet.y = random.nextInt(resY);
-                break;
-            case 3:
-                tempOffSet.x = resX;
-                tempOffSet.y = random.nextInt(resY);
-                break;
-        }
-        return tempOffSet;
-    }
-
-    private Point randDirection(Point oSet){
-        // pick where to generate
-        Point tempVect = new Point();
-        if(oSet.x <= resX/2){
-            tempVect.x = random.nextInt(resY / (scalar*4));
-        }else if(oSet.x > resX/2) {
-            tempVect.x = -random.nextInt(resX / (scalar*4));
-        }
-        if(oSet.y <= resY/2) {
-            tempVect.y = random.nextInt(resY / (scalar*5));
-        }else if(oSet.y > resY/2) {
-            tempVect.y = -random.nextInt(resX / (scalar*5));
-        }
-
-        return tempVect;
     }
 
 }
