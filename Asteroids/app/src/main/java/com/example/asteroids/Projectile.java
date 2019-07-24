96acd309f9832e11bb7083cf2553953ca79292d7
bcoe <briancoe@gmail.com>
2019-07-23 20:05:57 -0700

    Pushing before I goto bed will work more tomorrow

--- Asteroids/app/src/main/java/com/example/asteroids/Projectile.java
+++ Asteroids/app/src/main/java/com/example/asteroids/Projectile.java
@@ -16,27 +16,28 @@ public class Projectile extends MovableObject{
     //      CONSTRUCTOR
     ///////////////////////////
 
-    public Projectile(PointF blockSize, PointF speed, PointF direction, PointF Position1, PointF Position2, float rotation, long fps) {
+    public Projectile(PointF blockSize, PointF speed, PointF pos1,
+                      PointF pos2, float rotation) {
         // posX, posY, mass, maxVelocity, minVelocity, drctnVector are the parameters
         super(blockSize);
-        this.rotation = rotation;
         directionVector = new PointF();
-        directionVector.x = Position1.x - Position2.x;
-        directionVector.y = Position1.y - Position2.y;
-        float mag = (float)Math.sqrt(directionVector.x*directionVector.x
-                + directionVector.y*directionVector.y);
-        directionVector.x = (directionVector.x / mag);
-        directionVector.y = (directionVector.y/ mag);
-        shapeCoords = new PointF[3];
-        shapeCoords[0] = new PointF(Position1.x,Position1.y);
-        shapeCoords[1] = new PointF(Position1.x + 2 * directionVector.x,
-                Position1.y + 2 * directionVector.y);
-        shapeCoords[2] = new PointF(Position1.x,Position1.y);
-        startTime = System.currentTimeMillis();
-        mass = 100;
 
-        currVelocity.set(10 * directionVector.x,
-                10 * directionVector.y);
+        directionVector.x = ((float)Math.cos(rotation)*pos1.x);
+        directionVector.y = -((float)Math.sin(rotation)*pos1.y);
+        shapeCoords = new PointF[3];
+        shapeCoords[0] = new PointF(pos2.x, pos2.y);
+        shapeCoords[1] = new PointF(pos2.x + directionVector.x, pos2.y + directionVector.y);
+        shapeCoords[2] = new PointF(pos2.x, pos2.y);
+        startTime = System.currentTimeMillis();
+        mass = 10;
+        float mag = (float)Math.sqrt(directionVector.x*directionVector.x +
+                directionVector.y*directionVector.y);
+        directionVector.x = directionVector.x/mag;
+        directionVector.x = directionVector.y/mag;
+
+        currVelocity.set(directionVector.x,
+                directionVector.y);
+
 //        updatePhysics(fps, new PointF( (float)(speed.x + 10 * directionVector.x/mag),
   //              (float)(speed.x + 10 * directionVector.y/mag)));
 
