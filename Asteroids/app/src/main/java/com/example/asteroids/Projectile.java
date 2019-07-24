309e2f66aed76cb5e6aed9a2e16596aefe056740
bcoe <briancoe@gmail.com>
2019-07-24 14:17:21 -0700

    Merge branch 'master' of C:\Users\Brian\Desktop\New folder\One with conflicts.

--- Asteroids/app/src/main/java/com/example/asteroids/Projectile.java
+++ Asteroids/app/src/main/java/com/example/asteroids/Projectile.java
@@ -17,36 +17,23 @@ public class Projectile extends MovableObject{
     ///////////////////////////
 
     public Projectile(PointF blockSize, PointF speed, PointF pos1,
-                      PointF pos2, float rotate) {
+                      PointF pos2, float rotation) {
         // posX, posY, mass, maxVelocity, minVelocity, drctnVector are the parameters
         super(blockSize);
         directionVector = new PointF();
-        directionVector.x = ((float)Math.cos(rotate)*(pos2.x - pos1.x))-((float)Math.sin(rotate)*(pos2.y - pos1.y));
-        directionVector.y = ((float)Math.cos(rotate)*(pos2.y - pos1.y))+((float)Math.sin(rotate)*(pos2.x - pos1.x));
+
+        directionVector.x = ((float)Math.cos(rotation)*pos1.x);
+        directionVector.y = ((float)Math.sin(rotation)*pos1.y);
         shapeCoords = new PointF[3];
         shapeCoords[0] = new PointF(pos2.x, pos2.y);
         shapeCoords[1] = new PointF(pos2.x - directionVector.x, pos2.y - directionVector.y);
         shapeCoords[2] = new PointF(pos2.x, pos2.y);
         startTime = System.currentTimeMillis();
         mass = 10;
-
-        if(rotation < 90){
-            directionVector.x = ((float)Math.cos(rotate)*(pos2.x - pos1.x))-((float)Math.sin(rotate)*(pos2.y - pos1.y));
-            directionVector.y = -((float)Math.cos(rotate)*(pos2.y - pos1.y))+((float)Math.sin(rotate)*(pos2.x - pos1.x));
-        }else if(rotation < 180){
-            directionVector.x = ((float)Math.cos(rotate)*(pos2.x - pos1.x))-((float)Math.sin(rotate)*(pos2.y - pos1.y));
-            directionVector.y = -((float)Math.cos(rotate)*(pos2.y - pos1.y))+((float)Math.sin(rotate)*(pos2.x - pos1.x));
-        }else if(rotation < 270){
-            directionVector.x = ((float)Math.cos(rotate)*(pos2.x - pos1.x))-((float)Math.sin(rotation)*(pos2.y - pos1.y));
-            directionVector.y = -((float)Math.cos(rotate)*(pos2.y - pos1.y))+((float)Math.sin(rotate)*(pos2.x - pos1.x));
-        }else{
-            directionVector.x = ((float)Math.cos(rotate)*(pos2.x - pos1.x))-((float)Math.sin(rotate)*(pos2.y - pos1.y));
-            directionVector.y = -((float)Math.cos(rotate)*(pos2.y - pos1.y))+((float)Math.sin(rotate)*(pos2.x - pos1.x));
-        }
         float mag = (float)Math.sqrt(directionVector.x*directionVector.x +
                 directionVector.y*directionVector.y);
-        directionVector.x = directionVector.x*10;
-        directionVector.x = directionVector.y*10;
+        directionVector.x = directionVector.x/mag;
+        directionVector.x = directionVector.y/mag;
 
         currVelocity.set(directionVector.x,
                 directionVector.y);
