8fc49aac7ab9d861e10025ac27022989ee5f7fc5
bcoe <briancoe@gmail.com>
2019-07-24 19:10:33 -0700

    fixed bottom right shooting quadrant, projectiles ahd x and y flipped.

--- Asteroids/app/src/main/java/com/example/asteroids/Projectile.java
+++ Asteroids/app/src/main/java/com/example/asteroids/Projectile.java
@@ -30,8 +30,8 @@ public class Projectile extends MovableObject{
             directionVector.y = ((float)Math.cos(rotateRads)*(pos1.y - pos2.y))+((float)Math.sin(rotateRads)*(pos1.x - pos2.x));
         }else if(rotate > 90 && rotate <= 180){
             rotateRads += 3.14;
-            directionVector.y = ((float)Math.cos(rotateRads)*(pos2.x - pos1.x))-((float)Math.sin(rotateRads)*(pos2.y - pos1.y));
-            directionVector.x = ((float)Math.cos(rotateRads)*(pos2.y - pos1.y))+((float)Math.sin(rotateRads)*(pos2.x - pos1.x));
+            directionVector.x = ((float)Math.cos(rotateRads)*(pos2.x - pos1.x))-((float)Math.sin(rotateRads)*(pos2.y - pos1.y));
+            directionVector.y = -((float)Math.cos(rotateRads)*(pos2.y - pos1.y))+((float)Math.sin(rotateRads)*(pos2.x - pos1.x));
         }else if(rotate > 180 && rotate <= 270){
             rotateRads += 3.14;
             directionVector.x = ((float)Math.cos(rotateRads)*(pos2.x - pos1.x))-((float)Math.sin(rotateRads)*(pos2.y - pos1.y));
