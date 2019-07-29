c2e6d0bd399b4a73ebfb9b5b6f2b04c70e8e67f3
brian <briancoe@gmail.com>
2019-07-29 15:41:54 -0700

    readjusted projectile, now shoots properly from head of ship

--- Asteroids/app/src/main/java/com/example/asteroids/Projectile.java
+++ Asteroids/app/src/main/java/com/example/asteroids/Projectile.java
@@ -21,33 +21,23 @@ public class Projectile extends MovableObject{
         isHit = false;
         int addWx = 0;
         int addWy = 0;
-        float rotateRads = (float)(Math.toRadians(rotate) - 3.14/2);
+        float rotateRads = (float)(Math.toRadians(rotate));// + 3.14/2);
         directionVector = new PointF();
 
         if(rotate > 0  && rotate <= 90){
+            setDirectionVector(pos2, pos1, rotateRads);
             addWx =1;
-            directionVector.x = ((float)Math.cos(rotateRads)*(pos1.x - pos2.x))-((float)Math.sin(rotateRads)*(pos1.y - pos2.y));
-            directionVector.y = ((float)Math.cos(rotateRads)*(pos1.y - pos2.y))+((float)Math.sin(rotateRads)*(pos1.x - pos2.x));
         }else if(rotate > 90 && rotate <= 180){
+            setDirectionVector(pos2, pos1, rotateRads);
             addWy =1;
-            rotateRads += 3.14;
-            directionVector.x = ((float)Math.cos(rotateRads)*(pos2.x - pos1.x))-((float)Math.sin(rotateRads)*(pos2.y - pos1.y));
-            directionVector.y = -((float)Math.cos(rotateRads)*(pos2.y - pos1.y))+((float)Math.sin(rotateRads)*(pos2.x - pos1.x));
         }else if(rotate > 180 && rotate <= 270){
+            setDirectionVector(pos2, pos1, rotateRads);
             addWx =1;
-            rotateRads += 3.14;
-            directionVector.x = ((float)Math.cos(rotateRads)*(pos2.x - pos1.x))-((float)Math.sin(rotateRads)*(pos2.y - pos1.y));
-            directionVector.y = -((float)Math.cos(rotateRads)*(pos2.y - pos1.y))+((float)Math.sin(rotateRads)*(pos2.x - pos1.x));
         }else if(rotate > 270 && rotate <= 360){
+            setDirectionVector(pos2, pos1, rotateRads);
             addWy =1;
-            rotateRads += 3.14;
-            directionVector.x = ((float)Math.cos(rotateRads)*(pos2.x - pos1.x))-((float)Math.sin(rotateRads)*(pos2.y - pos1.y));
-            directionVector.y = -((float)Math.cos(rotateRads)*(pos2.y - pos1.y))+((float)Math.sin(rotateRads)*(pos2.x - pos1.x));
         }
-        float mag = (float)Math.sqrt(directionVector.x*directionVector.x +
-                directionVector.y*directionVector.y);
-        directionVector.x = directionVector.x/mag;
-        directionVector.y = directionVector.y/mag;
+
 
         shapeCoords = new PointF[5];
         shapeCoords[0] = new PointF(pos2.x, pos2.y);
@@ -64,6 +54,14 @@ public class Projectile extends MovableObject{
     //      METHODS
     ///////////////////////////
 
+    public void setDirectionVector(PointF pos1, PointF pos2, float rotateRads){
+        directionVector.x = ((float)Math.cos(rotateRads)*(pos2.x - pos1.x))-((float)Math.sin(rotateRads)*(pos2.y - pos1.y));
+        directionVector.y = ((float)Math.cos(rotateRads)*(pos2.y - pos1.y))+((float)Math.sin(rotateRads)*(pos2.x - pos1.x));
+        float mag = (float)Math.sqrt(directionVector.x*directionVector.x +
+                directionVector.y*directionVector.y);
+        directionVector.x = directionVector.x/mag;
+        directionVector.y = directionVector.y/mag;
+    }
 
     public void update(long fps){
 //        if(System.currentTimeMillis() - startTime  < 100)
