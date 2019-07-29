89cfc7434dcbfc8c0f1c29b49380655d671144f3
brian <briancoe@gmail.com>
2019-07-29 16:10:15 -0700

    refactored projectile to move math into methods

--- Asteroids/app/src/main/java/com/example/asteroids/Projectile.java
+++ Asteroids/app/src/main/java/com/example/asteroids/Projectile.java
@@ -19,29 +19,12 @@ public class Projectile extends MovableObject{
                       PointF pos2, float rotate) {
         super(blockSize);
         isHit = false;
-        float addWx = 0;
-        float addWy = 0;
+        PointF addW = new PointF(0,0);
         float rotateRads = (float)(Math.toRadians(rotate));
         directionVector = new PointF();
         setDirectionVector(pos2, pos1, rotateRads);
-
-        if(rotate >= 0  && rotate < 90){
-            addWx = 1/blockSize.x;
-        }else if(rotate >= 90 && rotate <= 180){
-            addWy = 1/blockSize.y;
-        }else if(rotate > 180 && rotate < 270){
-            addWx = -1/blockSize.x;
-        }else if(rotate >= 270 && rotate <= 360){
-            addWy = -1/blockSize.x;
-        }
-
-
-        shapeCoords = new PointF[5];
-        shapeCoords[0] = new PointF(pos2.x, pos2.y);
-        shapeCoords[1] = new PointF(pos2.x + directionVector.x, pos2.y + directionVector.y);
-        shapeCoords[2] = new PointF(pos2.x + directionVector.x, pos2.y + directionVector.y);
-        shapeCoords[3] = new PointF(pos2.x+addWx, pos2.y+addWy);
-        shapeCoords[4] = new PointF(pos2.x, pos2.y);
+        setWidth(rotate, addW);
+        setDraw(pos1, pos2, addW);
         startTime = System.currentTimeMillis();
         mass = 10;
 
@@ -51,7 +34,7 @@ public class Projectile extends MovableObject{
     //      METHODS
     ///////////////////////////
 
-    public void setDirectionVector(PointF pos1, PointF pos2, float rotateRads){
+    private void setDirectionVector(PointF pos1, PointF pos2, float rotateRads){
         directionVector.x = ((float)Math.cos(rotateRads)*(pos2.x - pos1.x))
                 -((float)Math.sin(rotateRads)*(pos2.y - pos1.y));
         directionVector.y = ((float)Math.cos(rotateRads)*(pos2.y - pos1.y))
@@ -62,12 +45,33 @@ public class Projectile extends MovableObject{
         directionVector.y = directionVector.y/mag;
     }
 
+    private void setDraw(PointF pos1, PointF pos2, PointF addW){
+        shapeCoords = new PointF[5];
+        shapeCoords[0] = new PointF(pos2.x, pos2.y);
+        shapeCoords[1] = new PointF(pos2.x + directionVector.x, pos2.y + directionVector.y);
+        shapeCoords[2] = new PointF(pos2.x + directionVector.x, pos2.y + directionVector.y);
+        shapeCoords[3] = new PointF(pos2.x+addW.x, pos2.y+addW.y);
+        shapeCoords[4] = new PointF(pos2.x, pos2.y);
+    }
+
+    private void setWidth(float rotate, PointF addW){
+        if(rotate >= 0  && rotate < 90){
+            addW.x = 1/blockSize.x;
+        }else if(rotate >= 90 && rotate <= 180){
+            addW.y = 1/blockSize.y;
+        }else if(rotate > 180 && rotate < 270){
+            addW.x = -1/blockSize.x;
+        }else if(rotate >= 270 && rotate <= 360){
+            addW.y = -1/blockSize.x;
+        }
+
+    }
+
     public void update(long fps){
-//        if(System.currentTimeMillis() - startTime  < 100)
-//            isHit = false;
         for(PointF s: shapeCoords){
             s.x += directionVector.x;
             s.y += directionVector.y;
+//            updatePhysics(fps, directionVector);
         }
     }
 }
