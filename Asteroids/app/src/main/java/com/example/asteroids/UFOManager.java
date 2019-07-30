abafe16861f0edfd46bde11966a3679ae7665b3d
Jose Torres-Vargas <josertrs@gmail.com>
2019-07-29 17:17:33 -0700

    Added documentation to UFOManager.java

--- Asteroids/app/src/main/java/com/example/asteroids/UFOManager.java
+++ Asteroids/app/src/main/java/com/example/asteroids/UFOManager.java
@@ -1,37 +1,54 @@
 package com.example.asteroids;
 import android.content.res.Resources;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.PointF;
 import android.graphics.Point;
 import android.util.Log;
 
+
+/*
+ * UFO_Type denotes the UFO difficulty.
+ * GREEN: Default state easy
+ * YELLOW: Medium difficulty.
+ * RED: Hard difficulty.
+ */
 enum UFO_Type{
-    GREEN, YELLOW, RED, NONE
+    GREEN, YELLOW, RED
 }
 
 
+/**
+ * UFOManager
+ *
+ * This class is in charge of managing the UFOs in the game.
+ * It holds all of the UFO objects in an array. Aside from holding
+ * them this class is in charge of "spawning" in new UFOs. It keeps
+ * track of how long the UFOs have been alive. It also manages
+ * the current difficulty of UFOs and allows them to be configured.
+ *
+ * @author  Jose Torres-Vargas
+ */
+
 public class UFOManager {
-    //Will hold all of the UFO's to be managed
     private UFO[] ufoArray;
     int maxUFO;
     private int alive;
     private int wantActive;
+
     //Time stuff
     private Timers timers;
     private long gapTime;
     private long lastTime = 0;
-    private long timeOut;
     private UFO_Type currentDifficulty;
 
     UFOManager(Point res, PointF blockSize, Resources resources,
                ProjectileManager projectileManager, SFXManager sfx,
                int maxUFO, int wantActive, long timeOut, long ufoGapTime){
+
         this.maxUFO = maxUFO;
         ufoArray = new UFO[this.maxUFO];
         this.wantActive = wantActive;
 
-        this.timeOut = timeOut;
         gapTime = ufoGapTime;
         timers = new Timers(maxUFO, timeOut);
 
@@ -43,8 +60,21 @@ public class UFOManager {
     }
 
 
+    /**
+     * This method spawns a UFO into the screen.
+     * It first checks to ensure that there is enough room
+     * to spawn a new UFO. It also checks to see if there are
+     * already a number of UFOs alive that env. wants(wantActive)
+     *
+     * The timing mechanism is to ensure that there is some time
+     * (gapTime) in between each UFO spawning. If we do find an
+     * available UFO we start a "timer" to keep track of how long it
+     * has been alive.
+     *
+     * @return -1 on failure to spawn UFO, else non-negative int is returned
+     * on success
+     */
     int spawnUFO(){
-        Log.d("spawnUFO: ", "Entering fcn");
         int ret;
         if(alive >= maxUFO){
             return -1;
@@ -73,6 +103,15 @@ public class UFOManager {
     }
 
 
+    /**
+     *  method updates all of our timers to see if any UFO has timed out.
+     *  If so UFO state is changed to leaving. After timers are updated the
+     *  UFOs are all updated. This is just calculating the new position of the
+     *  UFO. Update the number of alive UFOs as well in case a UFO has "died"
+     *
+     * @param fps : Frames per second
+     * @return
+     */
     int update(long fps){
 
         timers.updateTimers();
@@ -84,10 +123,6 @@ public class UFOManager {
             }
 
             if(timers.doneTimers[i]){
-                Log.d("update: " , "timer " + i + " : set to Leaving");
-                //Change state of ufo to LEAVING
-                Log.d("UFOLife: ", "ufo at " + i + " timed out state to DEAD");
-                //sfx.playExplosion();
                 ufoArray[i].state.setState(new LeavingState());
                 timers.resetTimer(i);
             }
@@ -99,7 +134,6 @@ public class UFOManager {
                 ufoArray[i].update(fps);
                 if (ufoArray[i].state.isAvailable()) {
                     alive--;
-                    Log.d("UFOLife: ", "decrementing alive: " + alive);
                 }
             }
         }
@@ -107,29 +141,50 @@ public class UFOManager {
     }
 
 
+    /**
+     * Finds availabe UFO. This means that any UFO that is not busy
+     * i.e. UFO in the WAITING state.
+     *
+     * @return  -1 on failure, else return index of array element
+     * that holds available UFO.
+     */
     private int findAvailableUFO(){
-        Log.d("findAvailableUFO: ", "entering");
-        int i;
-        for(i = 0; i < maxUFO; i++){
+
+        for(int i = 0; i < maxUFO; i++){
             if(ufoArray[i].state.isAvailable()){
                 ufoArray[i].state.setState(new ReadyState());
-                Log.d("UFOLife: ", "found UFO at " + i + " changing state to READY");
                 return i;
             }
         }
-        Log.d("UFOLife: ", "didn't find available UFO");
         return -1;
     }
 
 
+    /**
+     * @return array that holds all UFOs. Called by env.
+     */
     UFO[] getUFOS(){
         return ufoArray;
     }
 
+
+    /**
+     * called by env in order to set the current difficulty of UFOs
+     * that are being spawned.
+     *
+     * @param difficulty: UFO difficulty that we want current UFOs to spawn
+     *                    as.
+     */
     void setCurrentDifficulty(UFO_Type difficulty){
         currentDifficulty = difficulty;
     }
 
+
+    /**
+     * UFO configuration is done here.
+     * Change UFO speed, color, shooting frequency, etc.
+     * @param index: element in array that holds UFO to configure
+     */
     void configureUFO(int index){
 
         ufoArray[index].difficulty = currentDifficulty;
